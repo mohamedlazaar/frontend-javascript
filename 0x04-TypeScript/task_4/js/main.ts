eedf53f40c6d2148195d0300b072c5e2193660ee
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-12-15 14:51:59 +0100

    completing the task_4

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -7,3 +7,28 @@
 export const cpp = new Subjects.Cpp()
 export const java = new Subjects.Java()
 export const react = new Subjects.React()
+
+// Create and export a teacher object
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Miftah',
+  lastName: 'Adem',
+  experienceTeachingC: 10,
+}
+
+// Cpp Subject
+console.log('C++')
+cpp.setTeacher(cTeacher)
+console.log(cpp.getRequirements())
+console.log(cpp.getAvailableTeacher())
+
+// Java Subject
+console.log('Java')
+java.setTeacher(cTeacher)
+console.log(java.getRequirements())
+console.log(java.getAvailableTeacher())
+
+// React Subject
+console.log('React')
+react.setTeacher(cTeacher)
+console.log(react.getRequirements())
+console.log(react.getAvailableTeacher())
